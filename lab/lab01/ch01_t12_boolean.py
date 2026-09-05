ba9836f1e7fa830d759ed7e992d978cda0aa07cc
yiutung2025 <liangyaodong4@gmail.com>
2026-09-05 15:09:04 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -2,5 +2,5 @@
 # I'm an expert Python coder.
 # I'm 21 years old and I plan to program cool stuff forever.
 
-False = age_is_12 = False
-True = name_is_maria = True
+ age_is_12 = False
+True =name_is_maria = True
