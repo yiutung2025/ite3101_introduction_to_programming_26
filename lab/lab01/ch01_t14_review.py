b51c45d971349a7956bbc36a64dd068d8e013811
yiutung2025 <liangyaodong4@gmail.com>
2026-09-05 15:34:50 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -2,5 +2,5 @@ skill_completed = 'Python Syntax'
 exercises_completed = 13
 points_per_exercise = 5
 point_total = 100
-point_total += exercises_completed * oints_per_exercise
+point_total += exercises_completed * points_per_exercise
 print('I got ' + str(point_total + 'points!'))
