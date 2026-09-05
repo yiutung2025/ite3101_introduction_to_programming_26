dfa5e678394c76be993c14c57f6f80106482a10f
yiutung2025 <liangyaodong4@gmail.com>
2026-09-05 15:35:36 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -3,4 +3,4 @@ exercises_completed = 13
 points_per_exercise = 5
 point_total = 100
 point_total += exercises_completed * points_per_exercise
-print('I got ' + str(point_total + 'points!'))
+print('I got ' + str(point_total) + 'points!'))
