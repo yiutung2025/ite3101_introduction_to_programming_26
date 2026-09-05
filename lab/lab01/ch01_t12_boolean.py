c50cc8c3824ebab3daecd3cfe49edb766e54fa14
yiutung2025 <liangyaodong4@gmail.com>
2026-09-05 15:07:38 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,5 +1,5 @@
 # Hi! I'm Maria and I live in script.py.
 # I'm an expert Python coder.
 # I'm 21 years old and I plan to program cool stuff forever.
-False = age_is_12
+Falsealse = age_is_12
 True = name_is_maria
