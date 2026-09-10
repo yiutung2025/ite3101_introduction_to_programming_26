8404cac6d52d6c8d1d0b81ae41527bd8da8022e1
yiutung2025 <liangyaodong4@gmail.com>
2026-09-10 08:13:19 +0000

    lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,5 +1,5 @@
 # Assign your variables below, each on its own line!
-caesa = "Graham"
+caesar = "Graham"
 praline = "John"
 viking = "Teresa"
 # Put your variables above this line, and uncomment the following 3 line of codes.
