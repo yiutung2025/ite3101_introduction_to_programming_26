b4e13b3aa1b49b70609fcb8928bbab75a680de8b
yiutung2025 <liangyaodong4@gmail.com>
2026-09-10 08:10:07 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
 # Assign your variables below, each on its own line!
 caesa = "Graham"
 praline = "John"
-viking = ""
+viking = "Tresa"
 # Put your variables above this line, and uncomment the following 3 line of codes.
 # print(caesar)
 # print(praline)
