eeb8f1dff896c14fd0c4ad22bb7093d05f0c2c41
yiutung2025 <liangyaodong4@gmail.com>
2026-09-12 07:34:32 +0000

    lab/lab02/ch02_t11_printing_variables.py

--- lab/lab02/ch02_t11_printing_variables.py
+++ lab/lab02/ch02_t11_printing_variables.py
@@ -1,5 +1,5 @@
 """Assign the string "Ping!" to
 the variable the_machine_goes on
 line 5, then print it out on line 6!"""
-the_machine_goes on = "Ping!"
+the_machine_goes = "Ping!"
 print (the_machine_goes )
