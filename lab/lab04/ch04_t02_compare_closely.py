22019acc716dcd1a83ed1f7fd92f6fe26b8286dc
yiutung2025 <liangyaodong4@gmail.com>
2026-09-17 08:44:48 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -7,7 +7,7 @@ bool_one = True  # We did this one for you!
 bool_two = True
 
 # Set this to True if 19 <= 19 or to False if it is not.
-bool_three = None
+bool_three = True
 
 # Set this to True if -22 >= -18 or to False if it is not.
 bool_four = None
