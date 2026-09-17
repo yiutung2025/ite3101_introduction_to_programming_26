6b585346aa54ba597b0fe0127da716568b45d03a
yiutung2025 <liangyaodong4@gmail.com>
2026-09-17 08:11:43 +0000

    [skip actions] lab/lab03/ch03_t06_grand_finale.py

--- lab/lab03/ch03_t06_grand_finale.py
+++ lab/lab03/ch03_t06_grand_finale.py
@@ -3,4 +3,4 @@ from datetime import datetime
 now = datetime.now()
 
 print('%02d:%02d:%04d' % (now.hour, now.minute, now.second))
-print('%02d/%02d/%04d %02d:%02d:02d) % (now.month,now.day)
+print('%02d/%02d/%04d %02d:%02d:02d) % (now.month,now.day.now)
