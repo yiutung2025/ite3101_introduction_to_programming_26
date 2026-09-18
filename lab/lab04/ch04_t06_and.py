e292db36459c8f896b470f0f47df21de60f72b75
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 10:07:13 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -4,6 +4,6 @@ bool_two = -(-(-(-2))) == -2 and 4 >= 16 ** 0.5
 print(bool_two)
 bool_three = 19 % 4 != 300 / 10 / 10 and False
 print(bool_three)
-bool_four = None
+bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
 
 bool_five = None
