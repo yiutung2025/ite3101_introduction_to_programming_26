7e4a96f62495677b935d783aacdc8d210cfe8d97
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 14:13:05 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -4,13 +4,13 @@
 bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 print(bool_one)
 # Make me true!
-bool_two = 8 >= 4 or 7<10
+bool_two = 8 >= 4 or 7 < 10
 print(bool_two)
 # Make me false!
 bool_three = "tony" == "mike" or 4 == 10
 print(bool_three)
 # Make me true!
-bool_four =  9 == 9  not 0.8 == 0.08
+bool_four = 9 == 9 not 8 == 9
 print(bool_four)
 # Make me true!
 bool_five = 0 + 4 == 4 and 2 ** 2 == 4
