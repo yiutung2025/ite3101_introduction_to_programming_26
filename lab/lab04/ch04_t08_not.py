74753b4585033a0251b3ec3324aba3b21b00662d
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 10:15:16 +0000

    [skip actions] lab/lab04/ch04_t08_not.py

--- lab/lab04/ch04_t08_not.py
+++ lab/lab04/ch04_t08_not.py
@@ -3,7 +3,7 @@ print(bool_one)
 bool_two = not 3 ** 4 < 4 ** 3
 print(bool_two)
 bool_three = not 10 % 3 <= 10 % 2
-p
+print(bool_three)
 bool_four = not 3 ** 2 + 4 ** 2 != 5 ** 2
-
+print(bo)
 bool_five = not not False
