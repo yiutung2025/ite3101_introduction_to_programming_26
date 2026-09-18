6332961687c7ea41d278369a95c1dc1fba39267b
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 10:07:47 +0000

    lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -6,4 +6,4 @@ bool_three = 19 % 4 != 300 / 10 / 10 and False
 print(bool_three)
 bool_four = -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
 print(bool_four)
-bool_five = None
+bool_five = True and True
