aa1ec5f90961698a5cf9e0c0423ddd28207c26d0
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 10:10:36 +0000

    [skip actions] lab/lab04/ch04_t07_or.py

--- lab/lab04/ch04_t07_or.py
+++ lab/lab04/ch04_t07_or.py
@@ -2,8 +2,8 @@ bool_one = 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
 print(bool_one)
 bool_two = True or False
 print(bool_two)
-bool_three = None
-
+bool_three = 100 ** 0.5 >= 50 or False
+print(bo)
 bool_four = None
 
 bool_five = None
