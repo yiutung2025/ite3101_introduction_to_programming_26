c79aa9c5281b183785804f2ea8492aac1df320a2
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 09:54:25 +0000

    [skip actions] lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t03_compare_closelier.py
+++ lab/lab04/ch04_t03_compare_closelier.py
@@ -5,14 +5,14 @@ bool_one = (20 - 10) > 15  # We did this one for you!
 print(bool_one)
 # (10 + 17) == 3**16
 # Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
-bool_two = (10 + 17) == 3**16
+bool_two = ((10 + 17) == 3**16)
 print(bool_two)
 # 1**2 <= -1
-bool_three = 1**2 <= -1
+bool_three = (1**2 <= -1)
 print(bool_three)
 # 40 * 4 >= -4
-bool_four = 40 * 4 >= -4
-print(b)
+bool_four = (40 * 4 >= -4)
+print(bool_four)
 # 100 != 10**2
-bool_five = 100 != 10**2
-print(bool_one, bool_two, bool_three, bool_four, bool_five)
+bool_five = (100 != 10**2)
+print(bool_five)
--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -4,7 +4,7 @@
 bool_one = 3 < 5  # We already did this one for you!
 
 # Make me false!
-bool_two = None
+bool_two = 6 != 
 
 # Make me true!
 bool_three = None
