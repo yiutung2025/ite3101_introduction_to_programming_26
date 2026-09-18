30301d8a13c1a6207ac06facd0e0a03520bfb9d2
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 10:30:57 +0000

    lab/lab04/ch04_t09_this_n_that.py

--- lab/lab04/ch04_t09_this_n_that.py
+++ lab/lab04/ch04_t09_this_n_that.py
@@ -5,6 +5,6 @@ print(bool_two)
 bool_three = True and not (False or False)
 print(bool_three)
 bool_four = not not True or False and not True
-print(bool_five)
+print(bool_four)
 bool_five = False or not (True and True)
 print(bool_five)
