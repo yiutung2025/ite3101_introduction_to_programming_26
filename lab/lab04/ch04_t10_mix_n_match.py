c007b7748ecdb043071cc862a05e3ac42968bb33
yiutung2025 <liangyaodong4@gmail.com>
2026-09-18 14:06:41 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -10,7 +10,7 @@ print(bool_two)
 bool_three = "tony" == "mike" or 4 == 10
 print(bool_three)
 # Make me true!
-bool_four =  9 != 0.9 
+bool_four =  9 != 0.9 and 0.8 = 
 
 # Make me true!
 bool_five = None
