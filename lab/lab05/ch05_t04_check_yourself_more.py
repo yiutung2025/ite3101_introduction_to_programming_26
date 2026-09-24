0ae9d82465cdaa67ce23da33097524320b2e254a
yiutung2025 <liangyaodong4@gmail.com>
2026-09-24 08:54:14 +0000

    [skip actions] lab/lab05/ch05_t06_ay_b_c.py

--- lab/lab05/ch05_t04_check_yourself_more.py
+++ lab/lab05/ch05_t04_check_yourself_more.py
@@ -1,7 +1,7 @@
 print('Welcome to the Pig Latin Translator!')
 
 # Start coding here!
-original = input(‘Enter a word:")
+original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
     print(original)
