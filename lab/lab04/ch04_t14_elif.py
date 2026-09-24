6f5b74860fc6f919eaa2a69abf9e0a2485352ebd
yiutung2025 <liangyaodong4@gmail.com>
2026-09-24 07:24:21 +0000

    lab/lab04/ch04_t14_elif.py

--- lab/lab04/ch04_t14_elif.py
+++ lab/lab04/ch04_t14_elif.py
@@ -1,7 +1,7 @@
 def greater_less_equal_5(answer: int) -> int:
-    if answer > 5
+    if answer > 5:
         return 1
-    elif answer < 5
+    elif answer < 5:
         return -1
     else:
         return 0
