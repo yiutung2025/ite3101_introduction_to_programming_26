5a760f222039e7ec4d3c8a1d6b06926805ef7a8c
yiutung2025 <liangyaodong4@gmail.com>
2026-09-24 08:35:32 +0000

    [skip actions] lab/lab05/ch05_t04_check_yourself_more.py

--- lab/lab05/ch05_t04_check_yourself_more.py
+++ lab/lab05/ch05_t04_check_yourself_more.py
@@ -6,4 +6,7 @@ original = input("Enter a word:")
 if len(original) > 0 and original.isalpha():
     print(original)
 else:
+    if len(original) > 0 and original.isalpha():
+    print(original)
+else:
     print("empty")
