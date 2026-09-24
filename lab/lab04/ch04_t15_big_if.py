76c670312a4e7d0047b1f40eaa1e80257ac43396
yiutung2025 <liangyaodong4@gmail.com>
2026-09-24 07:31:48 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -4,7 +4,7 @@ def grade_converter(grade: int) -> str:
         return "A"
     elif 80 <= grade < 90 :
         return "B"
-    elif None:
+    elif 70 
         return "C"
     elif None:
         return "D"
